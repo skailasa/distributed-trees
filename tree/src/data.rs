1a3d8bbb842b378daa989c95ba8387c1a22abb58
Srinath Kailasa <srinathkailasa@gmail.com>
2021-10-15 16:32:27 +0100

    Rework unbalanced tree algorithm to be significantly faster with fewer allocations

--- tree/src/data.rs
+++ tree/src/data.rs
@@ -1,19 +1,23 @@
 use rand::Rng;
 
-use crate::morton::{Point, PointsVec};
+use crate::morton::{Point, Points};
 
 /// Generate random distribution of PointsVec in range [0, 1),
 /// for testing.
-pub fn random(npoints: u64) -> PointsVec {
+pub fn random(npoints: u64) -> Points {
     let mut range = rand::thread_rng();
 
-    let mut points: PointsVec = Vec::new();
+    let mut points: Points = Vec::new();
 
     for _ in 0..npoints {
         let x: f64 = range.gen();
         let y: f64 = range.gen();
         let z: f64 = range.gen();
-        points.push(Point(x, y, z));
+        let mut p = Point::default();
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        points.push(p);
     }
 
     points
